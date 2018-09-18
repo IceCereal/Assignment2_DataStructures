c47c1c9f6e238c0c27ee4b7e1c31cf15ef6a2020
IceCereal <ns_raghav@hotmail.com>
2018-09-18 21:36:12 +0530

    Include getch and clrscr and minor var fix

--- Problem1/Problem1.c
+++ Problem1/Problem1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <termios.h>
+#include <unistd.h>
+
+/* reads from keypress, doesn't echo */
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+
+void clrscr(){
+    printf("\e[1;1H\e[2J");
+}
+
 
 void main(){
     int BubbleArray[10];
@@ -12,9 +33,13 @@ void main(){
     for (lvMain = 0; lvMain < 10; ++lvMain){
         srand(randomSeed++);
 
-        BubbleSort[lvMain] = rand()%10;
-        SelectSort[lvMain] = BubbleSort[lvMain];
+        BubbleArray[lvMain] = rand()%10;
+        SelectArray[lvMain] = BubbleArray[lvMain];
     }
+
+    printf("Bubble Sort and Selection Sort Animated\tIceCereal");
+    getch();
+    clrscr();
 
     return;
 }
