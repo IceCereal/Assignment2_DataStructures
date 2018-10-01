3e242a11ce2aec8cf6df1611a27c1e44be96798d
IceCereal <ns_raghav@hotmail.com>
2018-10-01 18:50:13 +0530

    Begin InsVsSel

---
+++ Problem3/InsVsSel.c
@@ -0,0 +1,22 @@
+//To find out the location where Insertion Sort beats Selection Sort
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void insertionSort(int *, int);
+void selectionSort(int *, int);
+
+void main(){
+
+    printf("\nInsertion Sort vs Selection Sort - IceCereal\n");
+
+    clock_t start, end, InsertSortTime, SelectSortTime;
+
+    int randomSeed = 1;
+
+    
+
+
+
+
+}
